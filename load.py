8f3e0dd8dfbcce015c81ebddcfa4ce55b0cc6f3b
M B <isaprettycoolguy@gmail.com>
2015-11-21 15:55:33 -0500

    initial additions

---
+++ load.py
@@ -0,0 +1,13 @@
+__author__ = 'Mark'
+import csv
+import random
+
+
+def main():
+    reader = csv.DictReader(open("naughty_list.csv", "r"))
+    nlist = [row for row in reader]
+
+
+
+def popRandom():
+    pass
